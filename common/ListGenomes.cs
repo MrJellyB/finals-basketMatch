aaffb58db8774a7c340cb3ab454c0eec0dc6b29f
Shvacher Noam <shvachernoam@gmail.com>
2018-04-22 23:54:47 +0300

    added ListGenomes class with not yet implemented genome functions

---
+++ common/ListGenomes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace common
+{
+    public class ListGenomes : Genome
+    {
+        #region Public Methods
+
+        public override void Crossover()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override float FitnessFunction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initiate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Mutate()
+        {
+            throw new NotImplementedException();
+        }
+        
+        #endregion
+    }
+}
