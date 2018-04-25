fa5604cfe2eb68aa5b07768d069aa2800eb3d2df
MrJellyB <shvachernoam@gmail.com>
2018-04-25 12:24:34 +0300

    added data members and properties to ListGenome class and a CTOR

--- common/ListGenomes.cs
+++ common/ListGenomes.cs
@@ -1,9 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace common
 {
     public class ListGenomes : Genome
     {
+
+        #region Data Members
+
+        private Random m_seed;
+        public Random Seed
+        {
+            get { return m_seed;}
+            set { m_seed = value;}
+        }
+        
+        private int m_min;
+        public int Min
+        {
+            get { return m_min;}
+            set { m_min = value;}
+        }
+        
+        private int m_max;
+        public int Max
+        {
+            get { return m_max;}
+            set { m_max = value;}
+        }
+        
+        private List<object> m_list;
+        public List<object> List
+        {
+            get { return m_list;}
+            set { m_list = value;}
+        }
+        
+
+        #endregion
+
+        #region CTOR
+
+        public ListGenomes()
+        {
+
+        }
+
+        public ListGenomes(long length, int min, int max) 
+        {
+            this.Length = length;
+            this.Min = min;
+            this.Max = max;
+
+            // Generate the gene list
+            for (int i = 0; i < length; i++)
+            {
+                int nextVal = GenerateRandGeneVal(min,max);
+                this.m_list.Add(nextVal);
+            }
+        }
+
+        private int GenerateRandGeneVal(int min, int max)
+        {
+            return this.m_seed.Next(min, max);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void CopyGenes(Genome dest)
+        {
+
+        }
+
+        #endregion
+
         #region Public Methods
 
         public override void Crossover(Genome g)
@@ -18,7 +90,6 @@ namespace common
 
         public override void Initiate()
         {
-            throw new NotImplementedException();
         }
 
         public override void Mutate()
