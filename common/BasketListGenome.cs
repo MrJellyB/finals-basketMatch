901a8f888c4bc3470ace5c0c38e6048a6cbbab63
MrJellyB <shvachernoam@gmail.com>
2018-04-27 20:41:32 +0300

    added new class that represents individual basket

---
+++ common/BasketListGenome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace common
+{
+    public class BasketListGenome : ListGenomes
+    {
+        #region Data Members
+
+        private int[] m_weights;
+
+
+        #endregion
+
+        #region Properties
+
+        public int[] Weights
+        {
+            get { return m_weights; }
+            private set { m_weights = value; }
+        }
+
+        #endregion
+    }
+}
