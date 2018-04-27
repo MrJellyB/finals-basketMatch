8fc54f1a99d8c6b045528ace38d8fd4a0346c8d9
MrJellyB <shvachernoam@gmail.com>
2018-04-28 00:03:42 +0300

    implemented initial version of Fitness Function

--- common/BasketListGenome.cs
+++ common/BasketListGenome.cs
@@ -8,14 +8,14 @@ namespace common
     {
         #region Data Members
 
-        private int[] m_weights;
+        private float[] m_weights;
 
 
         #endregion
 
         #region Properties
 
-        public int[] Weights
+        public float[] Weights
         {
             get { return m_weights; }
             private set { m_weights = value; }
@@ -27,7 +27,7 @@ namespace common
 
         public BasketListGenome(long length) : base(length, 0, 100)
         {
-            this.m_weights = new int[length];
+            this.m_weights = new float[length];
             // TODO: insert here default weights list
         }
 
@@ -35,14 +35,36 @@ namespace common
 
         #region Public Methods
 
-        public void SetWeights(int[] weights)
+        public void SetWeights(float[] weights)
         {
-            
+            this.m_weights = new float[weights.Length];
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                SetWeight(weights[i], i);
+            }
         }
 
-        public void SetWeight(int weight, int valNum)
+        public void SetWeight(float weight, int valNum)
         {
-            
+            if (weight > 1 || weight < 0)
+            {
+                throw new Exception("the weight must be between 1 and 0");
+            }
+
+            this.m_weights[valNum] = weight; 
+        }
+
+        public override float FitnessFunction()
+        {
+            float productToReturn = 1f;
+
+            for (int i = 0; i < this.Length; i++)
+            {
+                productToReturn *= (this.m_weights[i] * (float)this.m_list[i]);
+            }
+
+            return productToReturn;
         }
 
         #endregion
