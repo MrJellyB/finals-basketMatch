2b64cf1e56e21b54ec27c5f383b5ed09b9304bc2
MrJellyB <shvachernoam@gmail.com>
2018-04-28 12:02:55 +0300

    created Population class with basic data members

---
+++ common/Population.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace common
+{
+    public class Population
+    {
+        #region Data Members
+
+        private int m_length;
+        private int m_crossoverPoint;
+        private int m_initialPopulationCount;
+        private int m_populationLimit;
+
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region CTOR
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        #endregion
+    }
+}
